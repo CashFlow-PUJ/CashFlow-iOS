5501695b93b075074ac4afefa774b7d6b6e193a7
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2023-07-23 17:56:20 -0500

    Xcode project created.

---
+++ CashFlow/CashFlow/CashFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowApp.swift
+//  CashFlow
+//
+//  Created by Cristóbal Castrillón Balcázar on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CashFlow/CashFlow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CashFlow
+//
+//  Created by Cristóbal Castrillón Balcázar on 23/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
