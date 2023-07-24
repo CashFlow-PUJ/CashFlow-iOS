d1d4d545ff1b3fb0afd8691c4dabc3bd82549eb1
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2023-07-23 19:20:49 -0500

    Image picker + camera controller added.

--- CashFlow/CashFlow/ContentView.swift
+++ CashFlow/CashFlow/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showSheet: Bool = false
+    @State private var showImagePicker: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    
+    @State private var image: UIImage?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                
+                Spacer()
+                
+                Image(uiImage: image ?? UIImage())
+                    .resizable()
+                    .frame(width: 350, height: 400)
+                    .cornerRadius(15)
+                
+                Spacer()
+                
+                Button(
+                    action: {
+                        self.showSheet = true
+                    },
+                    label: {
+                        Image(systemName: "camera.fill")
+                            .renderingMode(.original)
+                            .foregroundColor(.primary)
+                            .font(.title)
+                })
+                .actionSheet(isPresented: $showSheet) {
+                    ActionSheet(
+                        title: Text("Escanear recibo"),
+                        message: Text("Seleccione una foto"),
+                        buttons: [
+                            .default(Text("Galería")) {
+                                self.showImagePicker = true
+                                self.sourceType = .photoLibrary
+                            },
+                            .default(Text("Cámara")) {
+                                self.showImagePicker = true
+                                self.sourceType = .camera
+                            },
+                            .cancel(Text("Cancelar"))
+                        ]
+                    )
+                }
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(image: self.$image, isShown: self.$showImagePicker, sourceType: self.sourceType)
+        }
     }
 }
 
