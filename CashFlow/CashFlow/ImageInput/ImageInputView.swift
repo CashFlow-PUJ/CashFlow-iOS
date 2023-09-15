5d6db490f391c90559e237c18069560605ff08ad
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2023-09-15 16:22:16 -0500

    Added storyboard file and updated from sheet to fullScreenCover on ImageInputView.

--- CashFlow/CashFlow/ImageInput/ImageInputView.swift
+++ CashFlow/CashFlow/ImageInput/ImageInputView.swift
@@ -29,7 +29,7 @@ struct ImageInputView: View {
             }
             .padding()
         }
-        .sheet(isPresented: $showImagePicker) {
+        .fullScreenCover(isPresented: $showImagePicker){
             ImageInputViewControllerRepresentable()
         }
     }
