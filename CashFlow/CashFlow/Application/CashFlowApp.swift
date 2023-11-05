8d1c72d1e5c11a1b46e4e156ef359993168cb3f2
Angie Tatiana Peña <penap_at@javeriana.edu.co>
2023-11-05 10:18:44 -0500

    Auth Bug solved

--- CashFlow/CashFlow/Application/CashFlowApp.swift
+++ CashFlow/CashFlow/Application/CashFlowApp.swift
@@ -1,10 +1,3 @@
-//
-//  CashFlowApp.swift
-//  CashFlow
-//
-//  Created by Cristóbal Castrillón Balcázar on 23/07/23.
-//
-
 import SwiftUI
 import FBSDKCoreKit
 import FirebaseCore
@@ -12,79 +5,100 @@ import FirebaseAuth
 import GoogleSignIn
 
 enum Route: Hashable {
-    case transactionLog
-    case login
+  case transactionLog
+  case login
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        FBSDKCoreKit.ApplicationDelegate.shared.application(
-                    application,
-                    didFinishLaunchingWithOptions: launchOptions
-                )
-        return true
-    }
-    
-    func application(
-            _ app: UIApplication,
-            open url: URL,
-            options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-        ) -> Bool {
-            ApplicationDelegate.shared.application(
-                app,
-                open: url,
-                sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-                annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-            )
-        }
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    FBSDKCoreKit.ApplicationDelegate.shared.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
+    return true
+  }
+
+  func application(_ app: UIApplication,
+                   open url: URL,
+                   options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    ApplicationDelegate.shared.application(
+      app,
+      open: url,
+      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+    )
+    return true
+  }
 }
 
 class Coordinator: ObservableObject {
-    
+
     var appDIContainer = AppDIContainer()
     @Published var path = [Route]()
-    
+    @Published var currentRoute: Route? = nil
+
+    func setup() {
+        if Auth.auth().currentUser != nil {
+            self.currentRoute = .transactionLog
+        }
+    }
 }
+
+
 
 @main
 struct CashFlowApp: App {
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @ObservedObject var coordinator = Coordinator()
     var userProfile = UserProfile()
     let sharedData = SharedData()
+    
+    func setup() {
+        coordinator.setup()
+        sharedData.userId = Auth.auth().currentUser?.uid ?? ""
+    }
+    
     var body: some Scene {
         WindowGroup {
-            NavigationStack(path: $coordinator.path) {
-                LoginView()
-                    .preferredColorScheme(.light)
-                    .navigationDestination(for: Route.self) { route in
-                        Group {
-                            switch route {
-                            case .transactionLog:
-                                EntryLogView(coordinator: coordinator, sharedData: sharedData)
-                                    .preferredColorScheme(.light)
-                            case .login:
-                                LoginView()
-                                    .preferredColorScheme(.light)
+            NavigationStack {
+                VStack {
+                    if Auth.auth().currentUser != nil {
+                        EntryLogView(coordinator: coordinator, sharedData: sharedData)
+                            .preferredColorScheme(.light)
+                            .navigationBarBackButtonHidden(true)
+                            .navigationDestination(for: Route.self) { route in
+                                switch route {
+                                case .transactionLog:
+                                    EntryLogView(coordinator: coordinator, sharedData: sharedData)
+                                        .preferredColorScheme(.light)
+                                        .navigationBarBackButtonHidden(true)
+                                case .login:
+                                    EmptyView()
+                                }
                             }
-                        }
-                        .navigationBarBackButtonHidden(true)
+                    } else {
+                        LoginView()
+                            .preferredColorScheme(.light)
+                            .navigationBarBackButtonHidden(true)
+                            .navigationDestination(for: Route.self) { route in
+                                switch route {
+                                case .transactionLog:
+                                    EmptyView()
+                                case .login:
+                                    LoginView()
+                                        .preferredColorScheme(.light)
+                                        .navigationBarBackButtonHidden(true)
+                                }
+                            }
                     }
+                }
             }
+            .onAppear(perform: setup)
             .environmentObject(userProfile)
             .environmentObject(sharedData)
             .environmentObject(coordinator)
-            .onAppear {
-                if Auth.auth().currentUser != nil {
-                    // DEBUG PRINT
-                    print("USUARIO AUTENTICADO: ", Auth.auth().currentUser?.uid as Any)
-                    sharedData.userId = Auth.auth().currentUser!.uid
-                    coordinator.path.append(.transactionLog)
-                }
-            }
             .onOpenURL { url in
                 GIDSignIn.sharedInstance.handle(url)
             }
